1d25158ba0126ecb9aaa4c1030be973554a2803c
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2020-04-18 19:42:38 -0400

    First commit

---
+++ site-config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  // App Info
+  appstore_link         : "https://apps.apple.com/us/app/math-flash/id1449259588",                                  
+  playstore_link        : null,                                        
+  google_analytics_ID   : null,                                       
+  presskit_download_link: "https://drive.google.com/open?id=15snYbT4kLJ7bZATdA_EXkKsSJ1lCOTsa",                                        
+  video_or_screenshot   : "screenshot",                                 // "screenshot" or "video"
+  app_url               : "https://mathflash.netlify.app/",             // Domain of your website without path_prefix.
+  path_prefix           : "/",                                          // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "Math Flash",
+  app_price             : null,
+  app_tagline           : "Practice makes perfect",
+  app_keywords          : ["Math", "Flash cards", "Card", "Practice"],
+
+  // Personal Info
+  your_name              : "Ricky Witherspoon ",
+  your_link              : "https://rsw3.xyz",
+  your_city              : "New Hampshire",
+  email_address          : "richardwitherspoon3@gmail.com",
+  linkedin_username      : "Rspoon3",
+  facebook_username      : null,
+  instagram_username     : "Rspoon3",
+  twitter_username       : "Rspoon3",
+  github_username        : "Rspoon3",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "Simple",
+      description: "A beautiful, clean interface makes it easy to find and understand your health metrics.",
+      fontawesome_icon_name: "star",
+    },
+    {
+      title: "Awards",
+      description: "Get encourangement and earn rewards the more you practice",
+      fontawesome_icon_name: "award",
+    },
+    {
+      title: "Customizable",
+      description: "Customize many different aspects, including question amount, background color, and difficulty",
+      fontawesome_icon_name: "cogs",
+    },
+  ],
+  header_background             : "rgba(0, 0, 0, 0.2)",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "#2d385480",
+  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "#001929",
+  primary_text_color            : "#000",
+  content_width                 : "1170px",
+  font                          : `"Helvetica Neue", sans-serif`,
+  heading_color                 : "#71B5D9",
+  link_color                    : "#71B5D9",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_tagline_color             : "#ffffff",
+  feature_title_color           : "#ffffff",
+  feature_text_color            : "#999999",
+  feature_icons_foreground_color: "#4CAABE",
+  feature_icons_background_color: "#f6f6f6",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#e6e6e6",
+  footer_text_color             : "#888888",
+
+  //7ed0fb light blue hear
+  //3587f7 dark blue heart
+}
---
+++ src/markdown/change-log.md
@@ -0,0 +1,33 @@
+# Version 1.1
+
+## What’s New
+- Added confetti for when you do excellent!
+- Amount of problems completed now sync between devices
+- Stickers now sync between devices
+- If you tap on a sticker you will now be shown your progress towards it
+- You are now notified if you’ve unlocked a new sticker
+- Added a really cool new animation for showing how long a problem set took you
+- In App Purchases will now use your countries currency if outside of the US
+
+## Bug Fixes
+-  Small bug fixes and improvements
+
+
+# Version 1.0
+Math Flash is the easiest way to practice your math skills and is made for skill levels of all ages.
+
+
+Features Include:
+
+- Multiply, Divide, Add, and Subtract
+- Graded on your performance
+- Four levels of difficulty
+- Shows time it took per session
+- Eight problem amounts to choose from
+- Four different color themes
+- Sticker rewards
+- The ability to redo mistakes
+- Mix math operations in a single practice session
+- Hide the answers automatically
+- Practice specific numbers
+- Supports all devices in landscape and portrait
